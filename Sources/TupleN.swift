6eca672775a3cc2c8e98bfd767c834f308f4ee20
Jessy Catterwaul <jessy@catterwaul.com>
2024-05-21 08:01:58 -0400

    typealiases

---
+++ Sources/TupleN.swift
@@ -0,0 +1,8 @@
+/// A homogeneous 2-tuple.
+public typealias Tuple2<Element> = (Element, Element)
+
+/// A homogeneous 3-tuple.
+public typealias Tuple3<Element> = (Element, Element, Element)
+
+/// A homogeneous 4-tuple.
+public typealias Tuple4<Element> = (Element, Element, Element, Element)
