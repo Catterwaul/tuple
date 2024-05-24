9c893e2763f47abb1ab8b9642e4e6c0b935c870e
Jessy Catterwaul <jessy@catterwaul.com>
2024-05-23 23:43:00 -0400

    Change `map` to curry

--- Sources/map.swift
+++ Sources/map.swift
@@ -1,29 +1,65 @@
 /// Transform each element of a 2-tuple.
 @inlinable public func map<Element, Transformed>(
-  _ elements: Tuple2<Element>,
-  _ transform: (Element) throws -> Transformed
-) rethrows -> Tuple2<Transformed> {
-  try (transform(elements.0), transform(elements.1))
+  _ elements: Tuple2<Element>
+) -> ((Element) -> Transformed) -> Tuple2<Transformed> {
+  { transform in (transform(elements.0), transform(elements.1)) }
 }
+
+/// Transform each element of a 2-tuple.
+@_disfavoredOverload @inlinable public func map<Element, Transformed>(
+  _ elements: Tuple2<Element>
+) -> ((Element) throws -> Transformed) throws -> Tuple2<Transformed> {
+  { transform in try (transform(elements.0), transform(elements.1)) }
+}
+
+// MARK: - 3-tuple
 
 /// Transform each element of a 3-tuple.
 @inlinable public func map<Element, Transformed>(
-  _ elements: Tuple3<Element>,
-  _ transform: (Element) throws -> Transformed
-) rethrows -> Tuple3<Transformed> {
-  try append(
-    map(prefix(elements), transform),
-    transform(elements.2)
-  )
+  _ elements: Tuple3<Element>
+) -> ((Element) -> Transformed) -> Tuple3<Transformed> {
+  { transform in 
+    append(
+      map(prefix(elements))(transform),
+      transform(elements.2)
+    )
+  }
 }
+
+/// Transform each element of a 3-tuple.
+@_disfavoredOverload @inlinable public func map<Element, Transformed>(
+  _ elements: Tuple3<Element>
+) -> ((Element) throws -> Transformed) throws -> Tuple3<Transformed> {
+  { transform in
+    try append(
+      map(prefix(elements))(transform),
+      transform(elements.2)
+    )
+  }
+}
+
+// MARK: - 4-tuple
 
 /// Transform each element of a 4-tuple.
 @inlinable public func map<Element, Transformed>(
-  _ elements: Tuple4<Element>,
-  _ transform: (Element) throws -> Transformed
-) rethrows -> Tuple4<Transformed> {
-  try append(
-    map(prefix(elements), transform),
-    transform(elements.3)
-  )
+  _ elements: Tuple4<Element>
+) -> ((Element) -> Transformed) -> Tuple4<Transformed> {
+  { transform in
+    append(
+      map(prefix(elements))(transform),
+      transform(elements.3)
+    )
+  }
+}
+
+/// Transform each element of a 4-tuple.
+@_disfavoredOverload @inlinable public func map<Element, Transformed>(
+  _ elements: Tuple4<Element>
+) -> ((Element) throws -> Transformed) throws -> Tuple4<Transformed> {
+  { transform in
+    try append(
+      map(prefix(elements))(transform),
+      transform(elements.3)
+    )
+  }
 }
--- Tests/TestCase.swift
+++ Tests/TestCase.swift
@@ -5,13 +5,18 @@ final class TestCase: XCTestCase {
   func test_map() {
     let tuple4 = (0, 1, 2, 3)
     XCTAssert(
-      map(tuple4) { $0 + 1 } == (1, 2, 3, 4)
+      map(tuple4)() { $0 + 1 } == (1, 2, 3, 4)
     )
     XCTAssert(
-      map(prefix(tuple4)) { $0 + 1 } == (1, 2, 3)
+      map(prefix(tuple4))() { $0 + 1 } == (1, 2, 3)
     )
     XCTAssert(
-      map(prefix(suffix(tuple4))) { $0 + 1 } == (2, 3)
+      map(prefix(suffix(tuple4)))() { $0 + 1 } == (2, 3)
+    )
+
+    func add1(_ int: Int) throws -> Int { int + 1 }
+    XCTAssert(
+      try map(tuple4)(add1) == (1, 2, 3, 4)
     )
   }
 
