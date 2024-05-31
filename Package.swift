b230452936a3d9da45d635a70c14901ee1b955be
Jessy Catterwaul <jessy@catterwaul.com>
2024-05-31 11:25:51 -0400

    Add DocC plugin

--- Package.swift
+++ Package.swift
@@ -7,6 +7,9 @@ let name = "Tuplé"
 _ = Package(
   name: name,
   products: [.library(name: name, targets: [name])],
+  dependencies: [
+    .package(url: "https://github.com/apple/swift-docc-plugin", branch: "main")
+  ],
   targets: [
     .target(name: name),
     .testTarget(
