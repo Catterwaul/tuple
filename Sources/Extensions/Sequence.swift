bb31246f84bea54d5c56b7f592da7af313c12bae
Jessy Catterwaul <jessy@catterwaul.com>
2024-06-11 02:08:55 -0400

    Improve private `Sequence.tuplePrefix` error handling flow
    This eliminates reprocessing the latest errors and the need for explicit error typing.

--- Sources/Extensions/Sequence.swift
+++ Sources/Extensions/Sequence.swift
@@ -29,7 +29,7 @@ public extension Sequence {
 }
 
 private extension Sequence {
-  private func tuple2Prefix() throws(Error) -> (tuple: Vectuple2<Element>, getNext: () -> Element?) {
+  private func tuple2Prefix() throws(Error) -> (tuple: Tuple2, getNext: () -> Element?) {
     var iterator = makeIterator()
     let getNext = { iterator.next() }
 
@@ -40,29 +40,33 @@ private extension Sequence {
     return ((_0, _1), getNext)
   }
 
-  private func tuple3Prefix() throws(Error) -> (tuple: Vectuple3<Element>, getNext: () -> Element?) {
-    do throws(Error) {
-      let (tuple, getNext) = try tuple2Prefix()
+  private func tuple3Prefix() throws(Error) -> (tuple: Tuple3, getNext: () -> Element?) {
+    let tuple: Tuple2, getNext: () -> Element?
 
-      guard let element = getNext()
-      else { throw .incorrectElementCount(expected: 3, actual: 2) }
-
-      return (appending(tuple)(element), getNext)
+    do {
+      (tuple, getNext) = try tuple2Prefix()
     } catch .incorrectElementCount(_, let actual) {
       throw .incorrectElementCount(expected: 3, actual: actual)
     }
+
+    guard let element = getNext()
+    else { throw .incorrectElementCount(expected: 3, actual: 2) }
+
+    return (appending(tuple)(element), getNext)
   }
 
-  private func tuple4Prefix() throws(Error) -> (tuple: Vectuple4<Element>, getNext: () -> Element?) {
-    do throws(Error) {
-      let (tuple, getNext) = try tuple3Prefix()
+  private func tuple4Prefix() throws(Error) -> (tuple: Tuple4, getNext: () -> Element?) {
+    let tuple: Tuple3, getNext: () -> Element?
 
-      guard let element = getNext()
-      else { throw .incorrectElementCount(expected: 4, actual: 3) }
-
-      return (appending(tuple)(element), getNext)
+    do {
+      (tuple, getNext) = try tuple3Prefix()
     } catch .incorrectElementCount(_, let actual) {
       throw .incorrectElementCount(expected: 4, actual: actual)
     }
+
+    guard let element = getNext()
+    else { throw .incorrectElementCount(expected: 4, actual: 3) }
+
+    return (appending(tuple)(element), getNext)
   }
 }
